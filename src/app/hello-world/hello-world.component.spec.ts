04d7fe1bf6a7e3c2f8175bc194461325715c0a65
Chris Jones <cjones@pixitmedia.com>
2017-10-10 14:21:01 +0100

    refactor: provide service using using overridecomponent function

--- src/app/hello-world/hello-world.component.spec.ts
+++ src/app/hello-world/hello-world.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, getTestBed, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloWorldComponent } from './hello-world.component';
 import { HelloWorldService } from '../hello-world.service';
@@ -7,22 +7,30 @@ describe('HelloWorldComponent', () => {
   let component: HelloWorldComponent;
   let fixture: ComponentFixture<HelloWorldComponent>;
 
+  let helloWorldService: HelloWorldService;
+
   beforeEach(async(() => {
     TestBed
       .configureTestingModule({
-        providers: [
-          { provide: HelloWorldService, useValue: new HelloWorldService(null) }
-        ],
         declarations: [HelloWorldComponent]
       })
-      .compileComponents();
-  }));
+      .overrideComponent(HelloWorldComponent, {
+        set: {
+          providers: [
+            { provide: HelloWorldService, useFactory: () => new HelloWorldService(null) }
+          ]
+        }
+      })
+      .compileComponents().then(() => {
+        fixture = TestBed.createComponent(HelloWorldComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HelloWorldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        const testBed: TestBed = getTestBed();
+
+        helloWorldService = testBed.get(HelloWorldService);
+      });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
